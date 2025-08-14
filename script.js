cf3b6e40a7c5a1c854944e729d4bc5a8ba3bea78
AugustoSus26 <145451648+AugustoSus26@users.noreply.github.com>
2025-08-14 08:21:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
+
+const perguntas = [
+    {
+        enuncuado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = peerguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
