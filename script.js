d6643086390ddbbd479ca69f2bdf86331c87d1b8
AugustoSus26 <145451648+AugustoSus26@users.noreply.github.com>
2025-08-21 08:37:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-const caixaPrincipal = document.querySelector("caixa-principal");
-const caixaPerguntas = document.querySelector("caixa-perguntas");
-const caixaAlternativas = document.querySelector("caixa-alternativas");
-const caixaResultado = document.querySelector("caixa-resultado");
-const textoResultado = document.querySelector("texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enuncuado: "Pergunta 1",
+        enunciado: "No seu aniversário de 10 anos, você acaba acordando tarde para pegar seu Pokémon inicial e começar sua jornada como um treinador pokémon. Porém, ainda sim há 2 iniciais disponíveis para escolher. Qual você irá escolher?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Pikachu",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Eevee",
+                afirmacao: "afirmacao"
+            }
         ]
     },
     {
-
-        enunciado: "Pergunta 2",
+        enunciado: "Ao sair do laboratório e andar por um tempo, você se depara com uma bifurcação que pode levar á 2 lugares diferentes. O caminho da esquerda leva à um grande campo onde tem um forte presença de Pokémon de tipo planta, e o caminho da direita leva à uma praia com vários Pokémon de tipo água. Qual caminho você irá seguir?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Esquerda",
+            "Direita"
         ]
     },
     {
-
         enunciado: "Pergunta 3",
         alternativas: [
             "Alternativa 1",
@@ -29,7 +33,6 @@ const perguntas = [
         ]
     },
     {
-
         enunciado: "Pergunta 4",
         alternativas: [
             "Alternativa 1",
@@ -37,7 +40,6 @@ const perguntas = [
         ]
     },
     {
-
         enunciado: "Pergunta 5",
         alternativas: [
             "Alternativa 1",
@@ -51,6 +53,21 @@ let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta(){
-    perguntaAtual = peerguntas[atual];
+    perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+}
+
+mostraPergunta();
